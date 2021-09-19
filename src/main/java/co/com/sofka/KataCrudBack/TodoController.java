d4c08ff5946c7acd31592393cc2d8c27922eb21c
Darley36 <darleybest@gmail.com>
2021-09-18 21:30:59 -0500

    creation of Todo controller and Todo service

---
+++ src/main/java/co/com/sofka/KataCrudBack/TodoController.java
@@ -0,0 +1,7 @@
+package co.com.sofka.KataCrudBack;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TodoController {
+}
---
+++ src/main/java/co/com/sofka/KataCrudBack/TodoService.java
@@ -0,0 +1,7 @@
+package co.com.sofka.KataCrudBack;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TodoService {
+}
